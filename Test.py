b28c560e8f2c84cd45fa06a470a0703f2a87ddba
HaoranLi525 <141290138+HaoranLi525@users.noreply.github.com>
2023-09-13 17:28:05 +1000

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,6 +1,7 @@
 import os
 import torch
 import torch.nn.functional as F
+import torchvision.transforms as T
 from torchvision import transforms
 from torch import nn
 import numpy as np
@@ -13,17 +14,16 @@ from utils import transform as tr
 from tensorboardX import SummaryWriter
 import logging
 import torch.backends.cudnn as cudnn
+from PIL import Image
 
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--val_batchsize', type=int, default=4, help='evaluation batch size')
-parser.add_argument('--num_workers', type=int, default=0, help='number of workers for data_loader')
 parser.add_argument('--num_classes', type=int, default=10, help='number of iron types')
 parser.add_argument('--clip', type=float, default=0.5, help='gradient clipping margin')
 parser.add_argument('--decay_rate', type=float, default=0.1, help='decay rate of learning rate')
 parser.add_argument('--decay_epoch', type=int, default=100, help='every n epochs decay learning rate')
-parser.add_argument('--load', type=str, default=None, help='train from checkpoints')
+parser.add_argument('--load', type=str, default='/home/guest/Downloads/Iron/savedModel/Deeplab/Net_epoch_best.pth', help='train from checkpoints')
 parser.add_argument('--gpu_id', type=str, default='0', help='train use gpu')
 parser.add_argument('--data_root', type=str, default='./dataset', help='images root')
 parser.add_argument('--save_path', type=str, default='./savedModel/Deeplab/', help='the path to save model and log')
@@ -49,27 +49,37 @@ if opt.load is not None:
     model.load_state_dict(torch.load(opt.load))
     print('load model from ', opt.load, flush=True)
 
-val_data = dataset.SemData(split='eval', data_root=opt.data_root, transform=val_transform)
-val_loader = torch.utils.data.DataLoader(val_data, batch_size=opt.val_batchsize, shuffle=False, num_workers=opt.num_workers, pin_memory=True, sampler=None)
-
 global best_accuracy, best_epoch
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-metrics = StreamSegMetrics(10)
-metrics.reset()
 model.eval()
 correct_sample = 0
 total_sample = 0
+transform = T.Compose([
+    T.ToTensor(),
+    T.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
+])
+
+image = Image.open(opt.data_root).convert('RGB')
+image = transform(image)
+image = image.unsqueeze(0)
+total_number = 0
 with torch.no_grad():
-    for i, (image, gt) in enumerate(test_loader, start=1):
-        image = image.to(device, dtype=torch.float32)
-        gt = gt.to(device, dtype=torch.long)
-        res, _, _ = model(image)
-        preds = res.detach().max(dim=1)[1].cpu().numpy()
-        targets = gt.cpu().numpy()
-        metrics.update(targets, preds)
-
-    score = metrics.get_results()
-    mIou = score['Mean IoU']
-    acc = score['Overall Acc']
-
-print('mIou: {:.4f}, acc: {:.4f}.'.format(mIou, acc, ))
+    image = image.to(device, dtype=torch.float32)
+    res, _, _ = model(image)
+    preds = res.detach().max(dim=1)[1].cpu().numpy()
+    result_list = []
+    for i in np.unique(preds):
+        if i == 0:
+            result_list.append(0)
+        else:
+            result_list.append(np.sum(preds==i))
+    for value in result_list:
+        total_number += value
+    for i in range(10):
+        if i == 0:
+            pass
+        elif i < len(result_list):
+            percentage = result_list[i]/total_number
+            print('{}料型占比:{:.2f}'.format(i, percentage))
+        else:
+            print('{}料型占比:{:.2f}'.format(i, 0))
